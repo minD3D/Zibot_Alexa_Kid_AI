27ee62da02f170805a17c61161b528276c8c1597
[minD3D] <gomdori626@gmail.com>
2018-05-31 11:44:43 -0700

    fact-skill

---
+++ fact-skill/lambda/custom/index.js
@@ -0,0 +1,277 @@
+
+const GET_FACT_MESSAGE = 'Hello Im zibot! ';
+const data = [
+  'Minji is Zibot English version developer',
+  'Zibot is from China I can speak Mandarin, English, Korean',
+  'Leo made me 3 years before, He is really nice guy!',
+  'I can sing a song with you! ask me sing together',
+  'If you want to speak Mandarin just ask me',
+  'Maybe I can help you math homework! Its secret for only you and me',
+  'Zibot have several version check on Zibot website'
+];
+
+///////////////////////////////////////////////
+
+const SKILL_NAME = 'Hello Zibot';
+const languageStrings = {
+    en: {
+        translation: {
+            WELCOME: 'Hi I wake up! What do you want to do with me?',
+            HELP: 'Say Zibot fact to listen about zibot, or tell me story to listen about story',
+            ABOUT: 'I can speak English, Chinese, Korean! Ask me whatever you want!',
+            STOP: 'Okay, see you next time!',
+        },
+    },
+    // , 'de-DE': { 'translation' : { 'TITLE'   : "Local Helfer etc." } }
+};
+
+
+const Alexa = require('ask-sdk-core');
+const i18n = require('i18next');
+const sprintf = require('i18next-sprintf-postprocessor');
+const https = require('https');
+const StoryData = require('./story_source.js');
+// 1. Handlers ===================================================================================
+
+
+
+const LaunchHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+
+        return request.type === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const attributesManager = handlerInput.attributesManager;
+        const responseBuilder = handlerInput.responseBuilder;
+
+        const requestAttributes = attributesManager.getRequestAttributes();
+        const speechOutput = `${requestAttributes.t('WELCOME')} ${requestAttributes.t('HELP')}`;
+        return responseBuilder
+            .speak(speechOutput)
+            .reprompt(speechOutput)
+            .getResponse();
+    },
+};
+
+const GetNewFactHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return (request.type === 'IntentRequest'
+        && request.intent.name === 'GetNewFactIntent');
+  },
+  handle(handlerInput) {
+    const randomFact = cookbook.getRandomItem(data);
+    const speechOutput = GET_FACT_MESSAGE + randomFact;
+
+    return handlerInput.responseBuilder
+      .speak(speechOutput)
+      .getResponse();
+  },
+};
+
+function randomArrayElement(array) {
+    let i = 0;
+    i = Math.floor(Math.random() * array.length);
+    return (array[i]);
+}
+
+
+const MickeyStoryHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+
+        return request.type === 'IntentRequest' && request.intent.name === 'MickeyStoryIntent';
+    },
+    handle(handlerInput) {
+      const attributesManager = handlerInput.attributesManager;
+      const responseBuilder = handlerInput.responseBuilder;
+
+      const requestAttributes = attributesManager.getRequestAttributes();
+      const storys = randomArrayElement(StoryData.dataByType('long story'));
+      sessionAttributes.storys = storys.name;
+      const speechOutput = `I have Mickey mouse storys, Do you want to listen about, ${storys.name}?`;
+      return responseBuilder
+          .speak(speechOutput)
+          .reprompt(speechOutput)
+          .getResponse();
+    },
+};
+
+const YesHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+
+        return request.type === 'IntentRequest' && request.intent.name === 'AMAZON.YesIntent';
+    },
+    handle(handlerInput) {
+        const attributesManager = handlerInput.attributesManager;
+        const responseBuilder = handlerInput.responseBuilder;
+
+        const sessionAttributes = attributesManager.getSessionAttributes();
+        const StoryName = sessionAttributes.story;
+        const storysDetails = StoryData.dataByName(StoryName);////////////////////////////
+        const speechOutput = `This story is about ${storysDetails.name
+        }, ${storysDetails.description
+        } Did you know about this story?
+        <say-as interpret-as="interjection">bon appetit</say-as>`;
+
+        return responseBuilder
+            .speak(speechOutput)
+            .getResponse();
+    },
+};
+
+
+//
+// const AboutHandler = {
+//     canHandle(handlerInput) {
+//         const request = handlerInput.requestEnvelope.request;
+//
+//         return request.type === 'IntentRequest' && request.intent.name === 'AboutIntent';
+//     },
+//     handle(handlerInput) {
+//         const attributesManager = handlerInput.attributesManager;
+//         const responseBuilder = handlerInput.responseBuilder;
+//
+//         const requestAttributes = attributesManager.getRequestAttributes();
+//
+//         return responseBuilder
+//             .speak(requestAttributes.t('ABOUT'))
+//             .getResponse();
+//     },
+// };
+
+// const AttractionHandler = {
+//     canHandle(handlerInput) {
+//         const request = handlerInput.requestEnvelope.request;
+//
+//         return request.type === 'IntentRequest' && request.intent.name === 'AttractionIntent';
+//     },
+//     handle(handlerInput) {
+//         const request = handlerInput.requestEnvelope.request;
+//         const responseBuilder = handlerInput.responseBuilder;
+//
+//         let distance = 200;
+//         if (request.intent.slots.distance.value && request.intent.slots.distance.value !== "?") {
+//             distance = request.intent.slots.distance.value;
+//         }
+//
+//         const attraction = randomArrayElement(getAttractionsByDistance(distance));
+//
+//         const speechOutput = `Try ${
+//             attraction.name}, which is ${
+//             attraction.distance === '0' ? 'right downtown. ' : `${attraction.distance} miles away. Have fun! `
+//         }${attraction.description}`;
+//
+//         return responseBuilder.speak(speechOutput).getResponse();
+//     },
+// };
+
+
+const HelpHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+
+        return request.type === 'IntentRequest' && request.intent.name === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const attributesManager = handlerInput.attributesManager;
+        const responseBuilder = handlerInput.responseBuilder;
+
+        const requestAttributes = attributesManager.getRequestAttributes();
+        return responseBuilder
+            .speak(requestAttributes.t('HELP'))
+            .reprompt(requestAttributes.t('HELP'))
+            .getResponse();
+    },
+};
+
+const StopHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+
+        return request.type === 'IntentRequest'
+            && (request.intent.name === 'AMAZON.NoIntent'
+            || request.intent.name === 'AMAZON.CancelIntent'
+            || request.intent.name === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const attributesManager = handlerInput.attributesManager;
+        const responseBuilder = handlerInput.responseBuilder;
+
+        const requestAttributes = attributesManager.getRequestAttributes();
+        return responseBuilder
+            .speak(requestAttributes.t('STOP'))
+            .getResponse();
+    },
+};
+
+const SessionEndedHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+
+        return request.type === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+
+        return handlerInput.responseBuilder.getResponse();
+    },
+};
+
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        const request = handlerInput.requestEnvelope.request;
+
+        console.log(`Error handled: ${error.message}`);
+        console.log(` Original request was ${JSON.stringify(request, null, 2)}\n`);
+
+        return handlerInput.responseBuilder
+            .speak('Sorry, I can\'t understand the command. Please say again.')
+            .reprompt('Sorry, I can\'t understand the command. Please say again.')
+            .getResponse();
+    },
+};
+
+
+// 3. Helper Functions ==========================================================================
+
+
+const LocalizationInterceptor = {
+    process(handlerInput) {
+        const localizationClient = i18n.use(sprintf).init({
+            lng: handlerInput.requestEnvelope.request.locale,
+            overloadTranslationOptionHandler: sprintf.overloadTranslationOptionHandler,
+            resources: languageStrings,
+            returnObjects: true,
+        });
+
+        const attributes = handlerInput.attributesManager.getRequestAttributes();
+        attributes.t = function (...args) {
+            return localizationClient.t(...args);
+        };
+    },
+};
+
+// 4. Export =====================================================================================
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+exports.handler = skillBuilder
+    .addRequestHandlers(
+        LaunchHandler,
+        GetNewFactHandler,
+        //AboutHandler,
+        MickeyStoryHandler,
+        //MickeyFactHandler,
+        YesHandler,
+        HelpHandler,
+        StopHandler,
+        SessionEndedHandler
+    )
+    .addRequestInterceptors(LocalizationInterceptor)
+    .addErrorHandlers(ErrorHandler)
+    .lambda();
---
+++ fact-skill/lambda/custom/story_source.js
@@ -0,0 +1,80 @@
+"use strict";
+
+module.exports= {
+
+  dataByType: function dataByType(input){
+    const list = [];
+    for (let i = 0; i < data.storys.length; i += 1) {
+        if (data.storys[i].type.search(input) > -1) {
+            list.push(data.storys[i]);
+        }
+    }
+    return list;
+  },
+
+  dataByName: function dataByName(input){
+        var list;
+        for (let i = 0; i < data.storys.length; i += 1) {
+            if (data.storys[i].name.search(input) > -1) {
+                list=data.storys[i];
+            }
+    }
+        return list;
+    }
+}
+
+const data={
+    name: 'Mickey Mouse',
+    company: 'Disney',
+    storys: [
+        {
+            name: 'Mickey\'s toy story',
+            type: 'long story',
+            description: 'Mickey was a mouse. He had many toys. \
+            He used to spread them all around his room. “Mickey put your toys at their right place. Some guests are coming today,” \
+            said his mother. Mickey quickly started gathering his toys. Minnie mouse was Mickey’s friend.\
+            She came running. He called Minnie inside and they started playing. “Hey Mickey! Let’s go out and play,” Minnie said.\
+            Mickey ran to play and told  Minnie to wait.  After some time, the guests came. “Come in Mickey! The guests are here,” said mummy. \
+            The guests brought gifts for him. “This is for you Mickey,” uncle said. Mickey said thanks to Uncle and Aunty. \
+            Mickey opened the gifts. It was a beautiful toy train. “Come Minnie, let’s play!” Mickey said. Uncle tumbled over a toy. \
+            He fell down. “Oooh! Aaaah!” Uncle cried in pain. “Mickey, look your Uncle could have been seriously hurt,” said the angry mother.\
+            Mickey held both his ears and said sorry to Uncle.“That’s all right Mickey! Next time, remember to put your toys at the right place!”\
+            Uncle advised him. From then, Mickey Mouse always kept his toys at their places.'
+        },
+        {
+            name: 'Mickey\'s story two',
+            type: 'long story',
+            description: 'story 2'
+        },
+        {
+            name: 'Mickey\'s story three',
+            type: 'long story',
+            description: 'story 3'
+        },
+        {
+            name: 'Mickey\'s pet',
+            type: 'short story',
+            description: 'Mickey Mouse has a pet; a dog. The dog is called Pluto. Pluto has 5 puppies with Minnie\'s dog, Fifi.',
+        },
+        {
+            name: 'Mickey\'s friends',
+            type: 'short story',
+            description: 'Goofy, Donald Duck, Daisy Duck, Chip Dale, Clarice, Max, Clarabelle Cow, Horace Horsecollar etc.',
+        },
+        {
+            name: 'Mickey\'s spaces',
+            type: 'short story',
+            description: 'He started off as a rabbit',
+        },
+        {
+            name: 'Mickey\'s  relationship',
+            type: 'short story',
+            description: 'He\'s married to Minnie',
+        },
+        {
+            name: 'Mickey\'s name',
+            type: 'short story',
+            description: 'Walt Disney originally called Mickey “Mortimer Mouse”. His wife persuaded him to change it.',
+        }
+    ],
+};
